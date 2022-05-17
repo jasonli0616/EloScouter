b28f009f0b143fcf24b6ef9fefd7c0ed62ca8499
Jason <contact@jasonli0616.dev>
2022-05-17 14:34:57 -0400

    Match prediction functionality
    - Ability to predict a result in each column for each team
    
    For every column in each team, the outliers will be removed and the average will be calculated. Based on the positivity/negativity of the columns (eg. balls scored vs balls missed), a singular numeric value will be calculated for each team using the average of each column. This numeric value will serve as the ranking to predict a win or loss. The averaged column data will serve as the predicted match data.

--- PredictScouter/Team.py
+++ PredictScouter/Team.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 from . import columns
 
 class Team:
@@ -89,8 +92,58 @@ class Team:
 
                         columns_data[column_type].append(match_data)
 
-                # Positive / negative influence on team ranking (-1 or 1)
-                column_influence = columns.columns[column_type]
+        for column_name, column_data in columns_data.items():
+
+            # Remove outliers from data list, and calculate average
+            column_data_no_outliers = Team.remove_outliers(column_data)
+            column_average = round(sum(column_data_no_outliers) / len(column_data_no_outliers))
+            
+            print(column_name)
+            print(column_average)
+            print(column_data)
+            print('\n')
+
+
+    @staticmethod
+    def remove_outliers(array: list):
+        """
+        Remove all the outliers from an array using Numpy.
+
+        Reference:
+        https://www.adamsmith.haus/python/answers/how-to-remove-outliers-from-a-numpy-array-in-python
+
+        Parameters
+        ----------
+
+        array: list[int]
+            the array to remove outliers from
+
+        Returns
+        ----------
+
+        list[int]
+            the array with outliers removed
+        """
+
+        # Enforce array of ints
+        for element in array:
+            if not isinstance(element, int):
+                raise TypeError(f'Can only remove outliers from list of ints.')
+
+        np_array = np.array(array)
+        np_mean = np.mean(np_array)
+        np_std = np.std(np_array)
+
+        np_dist_from_mean = abs(np_array - np_mean)
+        outlier_deviation = 5
+        np_not_outlier = np_dist_from_mean < outlier_deviation * np_std
+        np_no_outliers = np_array[np_not_outlier]
+
+        # If list is empty (if no outliers), return the original list
+        if np_no_outliers.size == 0:
+            return array
+
+        return np_no_outliers.tolist()
 
 
     def __eq__(self, __o: object) -> bool:
